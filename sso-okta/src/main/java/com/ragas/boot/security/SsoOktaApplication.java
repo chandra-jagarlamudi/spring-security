f6ed51258fbbf7076996f8e54d05eef1a3c87637
Chandra Sekhar JAGARLAMUDI <chandra.jagarlamudi@gmail.com>
2018-01-17 15:09:46 -0600

    Initial commit

---
+++ sso-okta/src/main/java/com/ragas/boot/security/SsoOktaApplication.java
@@ -0,0 +1,24 @@
+package com.ragas.boot.security;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableOAuth2Sso
+@SpringBootApplication
+public class SsoOktaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoOktaApplication.class, args);
+	}
+	
+	@GetMapping("/")
+    public String echoTheUsersEmailAddress(Principal principal) {
+        return "Hey there! Your email address is: " + principal.getName();
+    }
+}
